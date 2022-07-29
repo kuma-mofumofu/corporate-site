3bdcf9a16f473952eef3a3c0100012803292586a
くま <toucyann.910@gmail.com>
2022-07-29 13:54:36 +0900

    スライダーのJavaScriptがうまく動かないのでコメントアウト

--- js/script.js
+++ js/script.js
@@ -50,8 +50,33 @@ $(function () {
 
 document.addEventListener('DOMContentLoaded', () => {//先に読み込むため必要
 
-  const startTime = Date.now()*1000;//現在の時間を取得(Date.now()で、1/1000秒を取得するので1000倍にして小数点以下切り捨て)
+  /*------以下の書き方だと１回しか経過時間を取れない（プログラムが１回しか走らないから）------*/
 
-  const time = (Date.now()*1000) - startTime;// 経過時間を秒で取得
+  // const startTime = Date.now()*1000;               //現在の時間を取得(Date.now()で、1/1000秒を取得するので1000倍にして小数点以下切り捨て)
+  // const time = (Date.now()*1000) - startTime;      // 経過時間を秒で取得
+
+  /*-----------------------------------------------------------------------------*/
+
+
+
+  /*------うまくclassが付かない-----------------------------------------------------*/
+
+  // const sliderTime = document.querySelector('#slider');
+
+  // let count = 0;//初期値として0を代入
+
+  // const countUp = () => {
+  //   count++;                                          //現在のcountの値に1を足す
+  // }
+
+  // setInterval(countUp,1000);                          //変数countUpの処理を1000ミリ秒後（＝1秒後）に繰り返し
+
+  // if(countUp % 2 === 0){                              //「%」は割り算の余り、2で割って余り0で、2の倍数ということ
+  //   sliderTime.classList.toggle('is-active');         //2秒ごとに「is-active」を付けて消すを繰り返し
+  // }
+
+  /*-----------------------------------------------------------------------------*/
+
+
 
 });
